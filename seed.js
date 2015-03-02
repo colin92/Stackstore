cc73a3971f24d6c148479b49457d411f35a7479f
Vincent Daranyi <vincent@daranyi.com>
2015-03-02 17:04:24 -0500

    Model and test seeding completed

---
+++ seed.js
@@ -0,0 +1,79 @@
+var mongoose = require('mongoose'), 
+	async = require('async'),
+	user = require('./server/db/models/user.js'),
+	models = require('./server/db/models/schemas.js');
+
+var data = {
+	Product: [
+		{
+			title: 'Jeff Koons',
+			description: 'Balloon Dog (Yellow), 1994-2000',
+			price: 10000,
+			category: 'Contemporary',
+			imageUrl: 'http://lorempixel.com/400/400'  
+		},
+		{
+			title: 'Damien Hirst',
+			description: 'Away from the Flock, 1994',
+			price: 5000,
+			category: 'Contemporary',
+			imageUrl:'http://lorempixel.com/400/400'		
+		},
+		{
+			title: 'Jeff Koons',
+			description: 'Michael Jackson and Bubbles, 1988',
+			price: 1000,
+			category: 'Contemporary',
+			imageUrl:'http://lorempixel.com/400/400'
+		}
+	],
+	Review: [
+		{
+			title: 'Beautiful',
+			body: '... but too expensive for me',
+			stars: 5, 
+			productId: '54f4d703416cb0d13f29fd3c',
+			userId: '54f4d703416cb0d13f29fd3c' 
+
+		}
+	],
+	Order: [
+		{
+			userId: '54f4d703416cb0d13f29fd3c',
+			sessionId: 'Something',
+			items: [{
+				productId: '54f4d703416cb0d13f29fd3c',
+				quantity: 1,
+				price: 5000
+			}]
+		}
+	],
+	User: [
+		{
+			email: 'vincent@daranyi.com'
+		}
+	]
+};
+
+
+
+mongoose.connection.on('open', function() {
+    mongoose.connection.db.dropDatabase(function() {
+        
+        console.log("Dropped old data, now inserting data");
+        async.each(Object.keys(data),
+            function(modelName, outerDone) {
+                async.each(data[modelName],
+                    function(d, innerDone) {
+                        models[modelName].create(d, innerDone);
+                    },
+                    outerDone
+                );
+            },
+            function(err) {
+                console.log("Finished inserting data");
+                console.log("Control-C to quit");
+            }
+        );
+    });
+});
---
+++ server/db/models/schemas.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/stackstore');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+	
+
+var productSchema = new mongoose.Schema({
+	title: {type: String, required: true, unique: true},
+	description: String,
+	price: Number,
+	category: {type: String, required: true},
+	imageUrl: {type: String}
+	// What type should photo be?
+});
+
+
+var orderSchema = new mongoose.Schema({
+	// Needs to either have a user OR session
+	userId: String,
+	sessionId: String,
+	items: [{productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
+			quantity: Number,
+			price: Number}]
+});
+
+
+var reviewSchema = new mongoose.Schema({
+	title: String,
+	body: {type: String, required: true, min: 10},
+	stars: Number, 
+	productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
+	userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = {
+	Product: mongoose.model('Product', productSchema),
+	Order: mongoose.model('Order', orderSchema),
+	Review: mongoose.model('Review', reviewSchema)
+};
+
+console.log('Done!');
