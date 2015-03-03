2a6ad1c3fdc644a5101b49136dba48455ba35c9e
Vincent Daranyi <vincent@daranyi.com>
2015-03-03 12:37:45 -0500

    Test route working

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,5 +1,18 @@
 'use strict';
 var router = require('express').Router();
-module.exports = router;
+var Model = require('../../db/models/schemas');
 
 router.use('/tutorial', require('./tutorial'));
+
+// Initial routes setup
+router.get('/category/:cat', function(req, res) {
+	var category = req.params.cat;
+	console.log(category);
+	
+	Model.Product.find({category: category}, function(err, products){
+		res.send(products);
+	});
+});
+
+
+module.exports = router;
